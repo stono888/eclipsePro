a9268179a7519e24826619176fac82c92aa73f05
stono888 <stono_wang@163.com>
2014-12-05 17:49:59 +0800

    dom

---
+++ DOMTest/src/com/srie/test/DOMTest.java
@@ -0,0 +1,13 @@
+package com.srie.test;
+
+import javax.xml.parsers.DocumentBuilderFactory;
+
+public class DOMTest {
+
+	public static void main(String[] args) {
+		//创建一个DocumentBuilderFactory对象
+		DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+		//创建一个DocumentBuilder对象
+	}
+
+}
