5766576fa98ea2cc39774235208134c6b9787676
stono888 <stono_wang@163.com>
2014-12-06 07:25:30 +0800

    dom attr

--- DOMTest/src/com/srie/test/DOMTest.java
+++ DOMTest/src/com/srie/test/DOMTest.java
@@ -1,18 +1,72 @@
 package com.srie.test;
 
+import java.io.IOException;
+
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.NamedNodeMap;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
 
 public class DOMTest {
 
-	public static void main(String[] args) throws Exception {
-		//创建一个DocumentBuilderFactory对象
+	public static void main(String[] args) {
+		// 创建一个DocumentBuilderFactory对象
 		DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
-		//创建一个DocumentBuilder对象
-		DocumentBuilder db = dbf.newDocumentBuilder();
-		//test
-		//test2
-		
+		// 创建一个DocumentBuilder对象
+		try {
+			// 创建一个DocumentBuilder对象
+			DocumentBuilder db = dbf.newDocumentBuilder();
+			// 通过DocumentBuilder对象的parse方法加载books.xml文件到当前项目下
+			Document document = db.parse("books.xml");
+			// 获取所有book节点的集合
+			NodeList bookList = document.getElementsByTagName("book");
+			// 通过nodelist的getLength()方法可以获得bookList的长度
+			System.out.println("一共有" + bookList.getLength() + "本书");
+			// 遍历每一个book节点
+			for (int i = 0; i < bookList.getLength(); i++) {
+				System.out.println("==============下面开始遍历第" + (i + 1)
+						+ "本书的内容===================");
+				// 通过bookList.getItem(i)获取一个book节点，nodeList的索引值从0开始
+				// Node book = bookList.item(i);
+				// //获取book节点的所有属性集合
+				// NamedNodeMap attrs = book.getAttributes();
+				// System.out.println("第"+(i +
+				// 1)+"本书共有"+attrs.getLength()+"个属性");
+				// // 遍历book的属性
+				// for(int j = 0; j<attrs.getLength(); j++){
+				// //通过item(index)方法获取book节点的某一个属性
+				// Node attr = attrs.item(j);
+				// //获取属性名
+				// attr.getNodeName();
+				// System.out.print("属性名："+attr.getNodeName());
+				// //获取属性值
+				// attr.getNodeValue();
+				// System.out.println("--属性值："+attr.getNodeValue());
+				// }
+				// 前提：book节点有且仅能有一个id属性
+				// 将book节点进行强制类型转换，转换成element
+				Element book = (Element) bookList.item(i);
+				// 通过getAttribute("id")方法获取属性值
+				String attrValue = book.getAttribute("id");
+				System.out.println("id属性的属性值为" + attrValue);
+				System.out.println("==============结束遍历第" + (i + 1)
+						+ "本书的内容===================");
+			}
+
+		} catch (ParserConfigurationException e) {
+			e.printStackTrace();
+		} catch (SAXException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
 	}
 
 }
