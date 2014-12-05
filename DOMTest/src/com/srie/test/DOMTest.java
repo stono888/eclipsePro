a60b9cd0e65e1758f7b461a191054f93c97862b7
stono888 <stono_wang@163.com>
2014-12-05 18:03:49 +0800

    documentbuilder

--- DOMTest/src/com/srie/test/DOMTest.java
+++ DOMTest/src/com/srie/test/DOMTest.java
@@ -1,13 +1,16 @@
 package com.srie.test;
 
+import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 
 public class DOMTest {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
 		//创建一个DocumentBuilderFactory对象
 		DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
 		//创建一个DocumentBuilder对象
+		DocumentBuilder db = dbf.newDocumentBuilder();
+		
 	}
 
 }
