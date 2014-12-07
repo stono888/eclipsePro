bb19ffb65b076f84fd3783dea1c837f5e6a710de
stono888 <stono_wang@163.com>
2014-12-07 10:37:53 +0800

    sax parser

--- SAXTest/src/com/srie/handler/SAXParserHandler.java
+++ SAXTest/src/com/srie/handler/SAXParserHandler.java
@@ -1,11 +1,25 @@
 package com.srie.handler;
 
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.ArrayList;
+
 import org.xml.sax.Attributes;
 import org.xml.sax.SAXException;
 import org.xml.sax.helpers.DefaultHandler;
 
+import com.srie.entity.Book;
+
 public class SAXParserHandler extends DefaultHandler {
 	int bookIndex = 0;
+	private String value = null;
+	private Book book = null;
+	private ArrayList<Book> bookList = new ArrayList<Book>();
+
+
+	public ArrayList<Book> getBookList() {
+		return bookList;
+	}
 
 	/**
 	 * 用来遍历xml文件中的开始标签
@@ -17,6 +31,7 @@ public class SAXParserHandler extends DefaultHandler {
 		super.startElement(uri, localName, qName, attributes);
 		// 开始解析book元素的属性
 		if (qName.equals("book")) {
+			book = new Book();
 			bookIndex++;
 			System.out.println("===========开始遍历第" + bookIndex
 					+ "本书的内容==========");
@@ -29,19 +44,23 @@ public class SAXParserHandler extends DefaultHandler {
 				System.out.print("book元素的第" + (i + 1) + "个属性名是："
 						+ attributes.getQName(i));
 				System.out.println("---属性值是：" + attributes.getValue(i));
+				if (attributes.getQName(i).equals("id")) {
+					book.setId(attributes.getValue(i));
+				}
 			}
 		} else if (!qName.equals("book") && !qName.equals("bookstore")) {
 			System.out.print("节点名是：" + qName);
 		}
+		// qName.equals("name")时，向book对象中setName
 	}
 
 	@Override
 	public void characters(char[] ch, int start, int length)
 			throws SAXException {
 		super.characters(ch, start, length);
-		String value = new String(ch, start, length);
+		value = new String(ch, start, length);
 		if (!value.trim().equals("")) {
-			System.out.println("---节点值是："+value);
+			System.out.println("---节点值是：" + value);
 		}
 	}
 
@@ -55,7 +74,30 @@ public class SAXParserHandler extends DefaultHandler {
 		super.endElement(uri, localName, qName);
 		// 判断是否针对一本书已经遍历结束
 		if (qName.equals("book")) {
+			//结束book标签的时候，进行清空；
+			bookList.add(book);
+			book = null;
 			System.out.println("========结束遍历第" + bookIndex + "本书的内容=========");
+		} else if (qName.equals("name")) {
+			// 先进行startElement，然后进行characters，然后到了endElement方法
+			book.setName(value);
+			/*
+			 * //use reflection Class c = Book.class; Method[] ms =
+			 * c.getMethods(); for (Method method : ms) { if
+			 * (method.getName().toUpperCase() .equals("SET" +
+			 * qName.toUpperCase())) { try { method.invoke(book, value); } catch
+			 * (IllegalArgumentException e) { e.printStackTrace(); } catch
+			 * (IllegalAccessException e) { e.printStackTrace(); } catch
+			 * (InvocationTargetException e) { e.printStackTrace(); } } }
+			 */
+		} else if (qName.equals("author")) {
+			book.setAuthor(value);
+		} else if (qName.equals("year")) {
+			book.setYear(value);
+		} else if (qName.equals("price")) {
+			book.setPrice(value);
+		} else if (qName.equals("language")) {
+			book.setLanguage(value);
 		}
 	}
 
--- SAXTest/src/com/srie/test/SAXTest.java
+++ SAXTest/src/com/srie/test/SAXTest.java
@@ -8,6 +8,7 @@ import javax.xml.parsers.SAXParserFactory;
 
 import org.xml.sax.SAXException;
 
+import com.srie.entity.Book;
 import com.srie.handler.SAXParserHandler;
 
 public class SAXTest {
@@ -21,6 +22,16 @@ public class SAXTest {
 			//创建一个SAXParserHandler对象
 			SAXParserHandler handler = new SAXParserHandler();
 			parser.parse("books.xml", handler);
+			System.out.println("~!~!~!共有"+handler.getBookList().size()+"本书");
+			for (Book book : handler.getBookList()) {
+				System.out.println(book);
+				System.out.println(book.getId());
+				System.out.println(book.getName());
+				System.out.println(book.getAuthor());
+				System.out.println(book.getYear());
+				System.out.println(book.getPrice());
+				System.out.println(book.getLanguage());
+			}
 		} catch (ParserConfigurationException e) {
 			e.printStackTrace();
 		} catch (SAXException e) {
